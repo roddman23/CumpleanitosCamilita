60603fd27e77d475700758d7987fa62b0254947d
rodmen <rodrigo.gasolero@gmail.com>
2024-07-28 23:29:59 -0300

    com

--- script.js
+++ script.js
@@ -1,18 +1,4 @@
 document.addEventListener('DOMContentLoaded', function() {
-    // Inicializa el calendario
-    var calendarEl = document.getElementById('calendar');
-
-    var calendar = new FullCalendar.Calendar(calendarEl, {
-        initialView: 'dayGridMonth',
-        events: [
-            {
-                title: 'Nuestro Aniversario',
-                start: '2023-08-15'
-            }
-        ]
-    });
-
-    calendar.render();
 
     // Inicializa los carousels
     $('.carousel-container').slick({
@@ -35,12 +21,11 @@ document.addEventListener('DOMContentLoaded', function() {
     var audio = document.getElementById('birthdaySong');
     audio.play();
 
-    // Lanza el confeti al cargar la página usando party.js
-    party.confetti(document.body, {
-        count: party.variation.range(200, 300),
-        spread: 360,
-        shapes: ["square", "circle"],
-        colors: ["#4b0082", "#320062", "#1a0033", "#ffffff", "#ecf0f1"]
+    // Lanza el confeti al cargar la página
+    confetti({
+        particleCount: 200,
+        spread: 160,
+        origin: { y: 0.6 }
     });
 });
 
